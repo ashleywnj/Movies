aacf2e1e67a9b640ebd48accefea0c3410c72919
ashleywnj <ashleywnj@gmail.com>
2014-02-17 06:45:43 -0500

    The begining of the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WhatMovie::Application.config.secret_key_base = '95eb33d25f92fc71d8491c2eb5844a9370053f621cc90f06388270c885db40f049efc544f13617653ed057d2bff44043f2189da600b3e86aec49f65c720b1bac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WhatMovie::Application.config.session_store :cookie_store, key: '_what_movie_session'
